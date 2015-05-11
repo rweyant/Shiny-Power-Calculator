502df8a5bc587723da7206cbca7fb89ce1ab4091
rweyant <dnlw0273@HQILL007902.no.trinity-health.org>
2015-05-11 09:02:04 -0400

    2015-05-11 update from FL

--- app.R
+++ app.R
@@ -5,74 +5,154 @@ library(shiny)
 library(shinydashboard)
 
 server <- function(input,output){
-#   output$powerPlot <- renderPlot({  
+
+  output$p1 <- renderUI({
+    if(input$test.type=='Proportions'){
+      sliderInput(inputId = 'p1',label = 'Sample Proportion 1',value = 0.3,min=0,max=1)
+    }else if(input$test.type=='Means'){
+      numericInput(inputId = 'x1',label = 'Sample Mean 1',value = 20)
+    }
+  })
+  output$var1 <- renderUI({
+    if(input$test.type=='Means'){
+      numericInput(inputId = 'v1',label = 'Sample Variance 1',value = 5)
+    }
+  })
+  output$p2 <- renderUI({
+    if(input$test.type=='Proportions'){
+      sliderInput(inputId = 'p2',label = 'Sample Proportion 2',value = 0.31,min=0,max=1)
+    }else if(input$test.type=='Means'){
+      numericInput(inputId = 'p1',label = 'Sample Mean ',value = 10)
+    }
+  })
+  output$var2 <- renderUI({
+    if(input$test.type=='Means'){
+      numericInput(inputId = 'v2',label = 'Sample Variance 2',value = 5)
+    }
+  })  
+  
+  # Select type of x-axis
+  output$xaxis <- renderUI({
+    if(input$type.of.calculation=='Power'){
+      radioButtons(inputId = 'xaxis',
+                   inline = T,
+                   selected = 'Type I Error',
+                   choices = c('Type I Error','Sample Size'),
+                   label = 'x-axis'
+      )
+    } else if(input$type.of.calculation=='Sample Size'){
+      radioButtons(inputId = 'xaxis',
+                   inline = T,
+                   selected = 'Type I Error',
+                   choices = c('Type I Error','Power'),
+                   label = 'x-axis'
+      )
+    }
+  })
+  
+  # Show Sample Size as groups if it is a power calculation and x-axis is type I error
+  output$sample.size.group <- renderUI({
+    if(input$type.of.calculation=='Power' & input$xaxis != 'Sample Size'){
+      checkboxGroupInput(inputId = 'n.groups',
+                         label = 'Sample Size',
+                         choices=c('100','1000','5000','10000','50000','100000'),
+                         inline = F,
+                         selected = c('100','1000','5000','10000','50000','100000'))
+    }
+  })
+  
+  # Show Type I Error groups if x-axis is either Sample Size or power
+  output$type.I.error.group <- renderUI({
+    if(input$xaxis != 'Type I Error'){
+      checkboxGroupInput(inputId = 'alpha.groups',
+                         label = 'Type I Error',
+                         choices=c('0.05','0.01','0.001','0.0001','0.0001','0.00001'),
+                         inline = F,
+                         selected = c('0.05','0.01','0.001','0.0001','0.0001','0.00001'))
+    }
+  })
+  # Show Power groups if type of calculation and x-axis is type I error
+  output$power.group <- renderUI({
+    if(input$type.of.calculation=='Sample Size' & input$xaxis != 'Power'){
+      checkboxGroupInput(inputId = 'power.groups',
+                         label = 'Power',
+                         choices=c('0.7','0.8','0.9','0.95','0.99'),
+                         inline = F,
+                         selected = c('0.7','0.8','0.9','0.95','0.99'))
+    }
+  })
+  
+  # Determine x-axis limits with double slider
+  output$xaxis.limits <- renderUI({
+    # Issue here [ERROR]
+    if(input$xaxis=='Type I Error'){
+      sliderInput(inputId = 'xaxis.limits',
+                  label = 'Type I Error',
+                  value=c(0.01,0.05),
+                  min = 0.0001, 
+                  max=0.05,
+                  step=0.0001)
+    } else if(input$xaxis=='Power'){
+      sliderInput(inputId = 'xaxis.limits',
+                  label = 'Power',
+                  value=c(0.8,0.95),
+                  min = 0.5, 
+                  max=1,
+                  step=0.01)
+    }else if(input$xaxis=='Sample Size'){
+      sliderInput(inputId = 'xaxis.limits',
+                  label = 'Sample Size',
+                  value=c(100,10000),
+                  min = 10, 
+                  max=100000,
+                  step=10)
+    }
+  })
+  
+  output$powerPlot <- renderText({
+    #class(input) %>% print
+    'test.type: ' %>% paste(input$test.type) %>% print
+    'type.of.calculation: ' %>% paste(input$type.of.calculation)  %>% print
+    'n.groups: ' %>% paste(input$n.groups) %>% print
+    'alpha.groups: ' %>% paste(input$alpha.groups) %>% print
+    'power.groups: ' %>% paste(input$power.groups) %>% print
+    'sample.size: ' %>% paste(input$sample.size) %>% print
+    'xaxis: ' %>% paste(input$xaxis) %>% print
+    'xaxis.limits ' %>% paste(input$xaxis.limits) %>% print  
+    print("\n\n\n-------")
 #     alpha <- 10^((-20:-1000)/10)
 #     n <- 10^((1:4)/1)
 #     x <- .05
 #     
 #     df <- data.frame(sapply(n,function(n1){ 
-#       sapply(alpha,function(x){ 
+#       sapply(alpha,function(x){
 #         power.prop.test(n=n1, 
 #                         p1=input$p1, 
 #                         p2=input$p2, 
-# #                         p1=0.3, 
-# #                         p2=0.4, 
+#                         #                         p1=0.3, 
+#                         #                         p2=0.4, 
 #                         sig.level=x, 
 #                         alternative='two.sided')$power})
-#       }))
+#     }))
 #     df$alpha <- alpha
+
+#     df$x <- .
+#     df$y <- .
+#     df$groups <- .
+#     df$xmin <- .
+#     df$xmax <- .
+
 #     colnames(df) <- c(n,'alpha')
 #     stacked.df <- gather(df,n,power,-alpha)
 #     stacked.df %>% head
 #     ggplot(stacked.df,aes(x=alpha,y=power,group=n,color=n,size=1.1))+
 #       geom_line()+
 #       scale_color_discrete()+
-# #       scale_size(range = c(1.1,1.1))+
 #       scale_size_continuous(guide=F,range = c(1.15,1.15))+
 #       guides(color=guide_legend(override.aes=list(size=2)))+
-# #       scale_x_log10()+
-# #   scale_x_reverse()+
-#       theme_classic()
-#     
-#   }),
-  output$samplesize <- renderUI({
-    if(input$calc=='power'){
-      checkboxGroupInput(inputId = 'n',
-                         label = 'Sample Size',
-                         choices=c('100','1000','5000','10000','50000','100000'),
-                         inline = T,
-                         selected = c('100','1000','5000','10000','50000','100000'),
-    }
+#       theme_classic()  
   })
-  output$alpha <- renderUI({
-    if(TRUE){
-      numericInput(inputId = 'alpha',label = 'Type I Error',value=0.05)
-    }
-  })
-  output$p1 <- renderUI({
-    if(input$test.type=='proportions'){
-      sliderInput(inputId = 'p1',label = 'Sample Proportion 1',value = 0.3,min=0,max=1)
-    }else if(input$test.type=='means'){
-      numericInput(inputId = 'x1',label = 'Sample Mean 1',value = 20)
-    }
-  })
-  output$var1 <- renderUI({
-    if(input$test.type=='means'){
-      numericInput(inputId = 'v1',label = 'Sample Variance 1',value = 5)
-    }
-  })
-  output$p2 <- renderUI({
-    if(input$test.type=='proportions'){
-      sliderInput(inputId = 'p2',label = 'Sample Proportion 2',value = 0.31,min=0,max=1)
-    }else if(input$test.type=='means'){
-      numericInput(inputId = 'p1',label = 'Sample Mean ',value = 10)
-    }
-  })
-  output$var2 <- renderUI({
-    if(input$test.type=='means'){
-      numericInput(inputId = 'v2',label = 'Sample Variance 2',value = 5)
-    }
-  })
+  
 }
 
 ui <- fluidPage(
@@ -80,24 +160,21 @@ ui <- fluidPage(
     sidebarPanel(
       radioButtons(inputId = 'test.type',
                    inline = T,
-                   selected = 'proportions',
-                   choices = c('means','proportions'),
-                   label = 'Type of Test'
+                   selected = 'Proportions',
+                   choices = c('Means','Proportions'),
+                   label = 'Test of '
                   ),
-      radioButtons(inputId = 'calc',
+      radioButtons(inputId = 'type.of.calculation',
                    inline = T,
-                   selected = 'power',
-                   choices = c('power','sample size'),
+                   selected = 'Power',
+                   choices = c('Power','Sample Size'),
                    label = 'Type of Calculation'
       ),
-      radioButtons(inputId = 'xaxis',
-                   inline = T,
-                   selected = 'Type I Error',
-                   choices = c('Type I Error','Sample Size'),
-                   label = 'Type of Calculation'
-      ),
-      uiOutput('samplesize'),
-      uiOutput('alpha'),
+      uiOutput('xaxis'),    
+      uiOutput('sample.size.group'),
+      uiOutput('type.I.error.group'),
+      uiOutput('power.group'),
+      uiOutput('xaxis.limits'),
       uiOutput('p1'),
       uiOutput('var1'),
       uiOutput('p2'),
